a2aa1989f751d2c1265660cdbb2b2d18afb8a807
sckimynwa <tsb03672@naver.com>
2022-06-01 14:02:39 +0900

    feat: add custom proto

--- node/dynamic_codegen/greeter_client.js
+++ node/dynamic_codegen/greeter_client.js
@@ -16,41 +16,45 @@
  *
  */
 
-var PROTO_PATH = __dirname + '/../../protos/helloworld.proto';
+var PROTO_PATH = __dirname + "/../../protos/helloworld.proto";
 
-var parseArgs = require('minimist');
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
+var parseArgs = require("minimist");
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function main() {
   var argv = parseArgs(process.argv.slice(2), {
-    string: 'target'
+    string: "target",
   });
   var target;
   if (argv.target) {
     target = argv.target;
   } else {
-    target = 'localhost:50051';
+    target = "localhost:50051";
   }
-  var client = new hello_proto.Greeter(target,
-                                       grpc.credentials.createInsecure());
+  var client = new hello_proto.Greeter(
+    target,
+    grpc.credentials.createInsecure()
+  );
   var user;
   if (argv._.length > 0) {
-    user = argv._[0]; 
+    user = argv._[0];
   } else {
-    user = 'world';
+    user = "world";
   }
-  client.sayHello({name: user}, function(err, response) {
-    console.log('Greeting:', response.message);
+  client.sayHello({ name: user }, function (err, response) {
+    console.log("Greeting:", response.message);
+  });
+  client.sayHelloAgain({ name: user }, function (err, response) {
+    console.log("Greeting:", response.message);
   });
 }
 
--- node/dynamic_codegen/greeter_server.js
+++ node/dynamic_codegen/greeter_server.js
@@ -16,25 +16,28 @@
  *
  */
 
-var PROTO_PATH = __dirname + '/../../protos/helloworld.proto';
+var PROTO_PATH = __dirname + "/../../protos/helloworld.proto";
 
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
-    });
+var grpc = require("@grpc/grpc-js");
+var protoLoader = require("@grpc/proto-loader");
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 /**
  * Implements the SayHello RPC method.
  */
 function sayHello(call, callback) {
-  callback(null, {message: 'Hello ' + call.request.name});
+  callback(null, { message: "Hello " + call.request.name });
+}
+
+function sayHelloAgain(call, callback) {
+  callback(null, { message: "Hello again, " + call.request.name });
 }
 
 /**
@@ -43,10 +46,14 @@ function sayHello(call, callback) {
  */
 function main() {
   var server = new grpc.Server();
-  server.addService(hello_proto.Greeter.service, {sayHello: sayHello});
-  server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
-    server.start();
-  });
+  server.addService(hello_proto.Greeter.service, { sayHello, sayHelloAgain });
+  server.bindAsync(
+    "0.0.0.0:50051",
+    grpc.ServerCredentials.createInsecure(),
+    () => {
+      server.start();
+    }
+  );
 }
 
 main();
